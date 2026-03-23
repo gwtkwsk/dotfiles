7e7020dd8f33e7828326a6e8d8374ca0cab32d9a
Grzegorz Witkowski <gmwitkowski@gmail.com>
2026-03-24 00:11:45 +0100

    Add neovim config

---
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    opts = {
+      contrast = "",
+      transparent_mode = false,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}

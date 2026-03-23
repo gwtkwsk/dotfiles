ede829efc701720405c6dd6459c68ede20def282
Grzegorz Witkowski <gmwitkowski@gmail.com>
2026-03-23 21:11:49 +0100

    Load aliases from external file

---
+++ dot_zshrc.d/aliases.zsh
@@ -0,0 +1,9 @@
+# >>> FOLDERS
+alias cdp="cd ~/Programowanie/Projekty/"
+# <<< FOLDERS
+
+# >>> GIT
+gitcl_gwtkwsk() { git clone "git@github.com_gwtkwsk:$1"; }
+gitcl_grzegorzwitkowski() { git clone "git@github.com_grzegorzwitkowski:$1"; }
+# <<< GIT
+

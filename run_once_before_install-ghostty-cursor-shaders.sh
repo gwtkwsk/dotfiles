4c504e86c69d30aeac1ef6aa139e7cc4d01f7ec7
Grzegorz Witkowski <gmwitkowski@gmail.com>
2026-04-04 00:49:05 +0200

    Clone shaders repo

---
+++ run_once_before_install-ghostty-cursor-shaders.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+GHOSTTY_DIR="$HOME/.config/ghostty"
+SHADERS_DIR="$GHOSTTY_DIR/shaders"
+
+mkdir -p "$GHOSTTY_DIR"
+
+if [ ! -d "$SHADERS_DIR" ]; then
+  git clone https://github.com/sahaj-b/ghostty-cursor-shaders "$SHADERS_DIR"
+fi

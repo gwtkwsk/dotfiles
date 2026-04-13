0825a9f985884de40a0bce658303c2adcf1fd0b8
Grzegorz Witkowski <grzegorz.witkowski@allegro.pl>
2026-04-13 12:53:37 +0200

    Add zoxide aliases

--- dot_zshrc.d/aliases.zsh
+++ dot_zshrc.d/aliases.zsh
@@ -2,6 +2,11 @@
 alias cdp="cd ~/Programowanie/Projekty/"
 # <<< FOLDERS
 
+# >>> ZOXIDE
+alias cd=z
+alias cdi=zi
+# <<< ZOXIDE
+
 # >>> GIT
 gitcl_gwtkwsk() { git clone "git@github.com_gwtkwsk:$1"; }
 gitcl_grzegorzwitkowski() { git clone "git@github.com_grzegorzwitkowski:$1"; }
